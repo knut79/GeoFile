b66a0d822270c8e4a296a930044d53584f94bfda
knut <knut.dullum@gmail.com>
2015-04-01 02:58:00 +0200

    first commit

---
+++ GeoFile/FilesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FilesViewController.swift
+//  GeoFile
+//
+//  Created by knut on 23/03/15.
+//  Copyright (c) 2015 knut. All rights reserved.
+//
+
+import Foundation
---
+++ GeoFile/PictureHelper.swift
@@ -0,0 +1,9 @@
+//
+//  PictureHelper.swift
+//  GeoFile
+//
+//  Created by knut on 29/03/15.
+//  Copyright (c) 2015 knut. All rights reserved.
+//
+
+import Foundation
